dd26dd85ad61c38f4d009726b82d54e31213e744
MarcosAllysson <marcosgracip05@gmail.com>
2021-09-27 19:39:40 -0300

    Files commit

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from comentarios.models import Comentario
+from comentarios.actions import aprova_comentarios, reprova_comentarios
 
-admin.site.register(Comentario)
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'comentario', 'data', 'aprovado')
+    actions = [aprova_comentarios, reprova_comentarios]
